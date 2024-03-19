abb676614a610b9131d28377e681bb802be70368
Quyen Luc <lucquyenw@gmail.com>
2024-03-19 21:42:50 +0700

    init repository

---
+++ db/sqlc/entry_test.go
@@ -0,0 +1,59 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"testing"
+	"time"
+
+	"github.com/lucquyenw/simplebank/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomEntry(t *testing.T) Entry {
+	account1 := CreateRandomAccount(t)
+
+	arg := CreateEntryParams{
+		AccountID: account1.ID,
+		Amount:    util.RandomMoney(),
+	}
+
+	entry, err := testQueries.CreateEntry(context.Background(), arg)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, entry.AccountID, account1.ID)
+	require.Equal(t, entry.Amount, arg.Amount)
+	return entry
+}
+
+func TestCreateEntry(t *testing.T) {
+	createRandomEntry(t)
+}
+
+func TestGetEntry(t *testing.T) {
+	entry1 := createRandomEntry(t)
+
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.ID, entry2.ID)
+	require.Equal(t, entry1.AccountID, entry2.AccountID)
+	require.Equal(t, entry1.Amount, entry2.Amount)
+	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
+}
+
+func TestDeleteEntry(t *testing.T) {
+	entry1 := createRandomEntry(t)
+
+	testQueries.DeleteEntry(context.Background(), entry1.ID)
+
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, entry2)
+}
